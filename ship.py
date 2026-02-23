e0ce7fb664a4be81bc9857bb6ba09b3c8ceef1d4
cer-apple <akaiapple1105@gmail.com>
2026-02-22 21:29:22 -0600

    Experiment: tested ship_speed=10(very fast), reverted to 1.5

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,7 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
@@ -22,9 +24,11 @@ class Ship:
     def update(self):
       """Update the ship's position based on movement flag."""
       if self.moving_right:
-        self.rect.x += 1
+        self.x += self.settings.ship_speed
       if self.moving_left:
-        self.rect.x -= 1
+        self.x -= self.settings.ship_speed
+        
+      self.rect.x = self.x
             
     def blitme(self):
       """Draw the ship at its current location."""
