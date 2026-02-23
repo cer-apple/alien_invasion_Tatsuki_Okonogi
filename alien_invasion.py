40f1e30ddbe5476e9b77d75ea214e964f07f33ef
cer-apple <akaiapple1105@gmail.com>
2026-02-22 18:22:40 -0600

    Ch12: Added Settings class and refactored screen settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,5 @@
+from settings import Settings
+
 import sys
 import pygame
 
@@ -8,10 +10,9 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -20,7 +21,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
