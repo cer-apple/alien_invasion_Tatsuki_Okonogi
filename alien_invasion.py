1851d926d5ce5ae9134524cd877a621ffbf76d53
cer-apple <akaiapple1105@gmail.com>
2026-02-22 20:18:46 -0600

    Refactor run_game(): extract _check_events() and _update_screen()

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,15 +20,18 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+            self._update_screen()
     
     def _check_events(self):
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    
+    def _update_screen(self):
+      """Update images on the screen, and flip to the new screen"""
+      self.screen.fill(self.settings.bg_color)
+      self.ship.blitme()
+      pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
