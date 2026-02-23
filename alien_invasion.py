e18b797746270be4065dbd5d6ad0e8c88d640313
cer-apple <akaiapple1105@gmail.com>
2026-02-22 19:48:57 -0600

    Add Ship class and display ship image on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,4 @@
+from ship import Ship
 from settings import Settings
 
 import sys
@@ -13,6 +14,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -22,6 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 if __name__ == '__main__':
