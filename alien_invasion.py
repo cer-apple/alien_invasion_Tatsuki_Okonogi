be8e00105aeb9429f29d055d9186ef2618ccc314
cer-apple <akaiapple1105@gmail.com>
2026-02-27 12:30:29 -0600

    Added nested loops to create full alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,23 +83,34 @@ class AlienInvasion:
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
     
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
         """Create an alien and place it in the row."""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
     
     def _create_fleet(self):
       """Create the fleet of aliens."""
-
+      # Create an alien and keep adding aliens until there's no room left.
+      # Spacing between aliens is one alien width and one alien height.
       alien = Alien(self)
-      alien_width = alien.rect.width
-      current_x = alien_width
+      alien_width, alien_height = alien.rect.size
 
-      while current_x < (self.settings.screen_width - 2 * alien_width):
-        self._create_alien(current_x)
-        current_x += 2 * alien_width
+      # Start position for first alien.
+      current_x, current_y = alien_width, alien_height
+
+      # Outer loop: rows (y)
+      while current_y < (self.settings.screen_height - 3 * alien_height):
+        # Inner loop: columns (x)
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            self._create_alien(current_x, current_y)
+            current_x += 2 * alien_width
+
+        # Finished a row; reset x value, and increment y value.
+        current_x = alien_width
+        current_y += 2 * alien_height
 
 if __name__ == '__main__':
     ai = AlienInvasion()
