a6310775d3373c7463a722779495b914ed96ff14
cer-apple <akaiapple1105@gmail.com>
2026-02-27 18:26:01 -0600

    Ch-14 Reset game state when Play is clicked

--- alien_invasion.py
+++ alien_invasion.py
@@ -201,7 +201,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
       """Start a new game when the player clicks Play."""
       if self.play_button.rect.collidepoint(mouse_pos) and not self.game_active:
+        # Reset the game statistics.
+        self.stats.reset_stats()
         self.game_active = True
+        
+        # Get rid of any remaining bullets and aliens.
+        self.bullets.empty()
+        self.aliens.empty()
+        
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
